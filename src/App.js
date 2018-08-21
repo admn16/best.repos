dfe6b8c9bc7a84a83fba915b9a21b4f698b87721
Anton Nocum <anton.nocum@gmail.com>
2018-08-21 18:04:22 +0800

    Added src/ as node_path

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from 'views/Home/Home';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit&nbsp;
-      <code>src/App.js</code>
-      &nbsp;and save to reload.
-    </p>
-  </div>
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        <Route exact path="/" component={Home} />
+      </p>
+    </div>
+  </Router>
 );
 
 export default App;
