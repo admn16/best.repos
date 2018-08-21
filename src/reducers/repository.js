180d7d6fe43431f3da9619ce3490459b6f61489a
Anton Nocum <anton.nocum@gmail.com>
2018-08-21 21:32:43 +0800

    Added loading:true on fetchRepositories

--- src/reducers/repository.js
+++ src/reducers/repository.js
@@ -3,12 +3,13 @@ import * as actionTypes from 'actions/repositoryActionTypes';
 const defaultState = {
   list: [],
   selected: {},
+  loading: false,
 };
 
 const repository = (state = defaultState, action) => {
   switch (action.type) {
     case [actionTypes.FETCH_REPOSITORIES]:
-      return state;
+      return { ...state, loading: true };
 
     default:
       return state;
